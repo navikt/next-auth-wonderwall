a8b230cd783e78eb84d966841b49b756e7cf9679
Karl J. Overå <k@rl.run>
2022-09-19 14:50:35 +0200

    add tests for idporten token validation using testcontainers

--- src/auth/shared/utils.ts
+++ src/auth/shared/utils.ts
@@ -1,4 +1,10 @@
-export type ValidationResult<ErrorTypes extends string> = 'valid' | { errorType: ErrorTypes; message: string };
+export type ValidationResult<ErrorTypes extends string> = 'valid' | ValidationError<ErrorTypes>;
+
+export type ValidationError<ErrorTypes extends string> = {
+    errorType: ErrorTypes;
+    message: string;
+    error?: Error | unknown;
+};
 
 export type GrantError = {
     errorType: 'OIDC_OP_RP_ERROR' | 'OIDC_UNKNOWN_ERROR' | 'UNKNOWN_ERROR' | 'NO_TOKEN';
